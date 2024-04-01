7dbd7ace859d6b1a8a369802011720ad8348bf77
daniel405 <danielfelipeg85@gmail.com>
2024-04-01 01:14:00 -0500

    second commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"hLdyM3uXswHmIwhuyKszObB8IZmqVT2e9KZNmlSWe10=\"\n}"
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss';
+const colors = require('tailwindcss/colors');
+
+const config: Config = {
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    colors: {
+      primary: '#EBB093',
+      secondary: '#8AADA3',
+      tertiary: '#2A354B',
+      darker: '#191B27',
+      ...colors,
+    },
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
